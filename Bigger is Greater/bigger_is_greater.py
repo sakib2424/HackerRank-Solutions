321d5d3bfd162abfcd476ebf1d9fc261de6fcbea
Ashfaque Sakib <as10239@stern.nyu.edu>
2020-06-11 01:37:59 -0400

    just started this

---
+++ Bigger is Greater/bigger_is_greater.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the biggerIsGreater function below.
+def biggerIsGreater(w):
+    return
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input())
+
+    for T_itr in range(T):
+        w = input()
+
+        result = biggerIsGreater(w)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
