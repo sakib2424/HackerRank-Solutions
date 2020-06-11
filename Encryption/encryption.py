8578bb8078a94e7ec4dcaa50b5f34644188114fd
Ashfaque Sakib <as10239@stern.nyu.edu>
2020-06-11 01:20:56 -0400

    Solution working, test cases passed

--- Encryption/encryption.py
+++ Encryption/encryption.py
@@ -8,15 +8,31 @@ import sys
 
 # Complete the encryption function below.
 def encryption(s):
+    string_length = len(s)
+    row = math.floor(math.sqrt(string_length))
+    column = math.ceil(math.sqrt(string_length))
+    if (row*column < string_length):
+        row += 1
+    matrix = []
+    for i in range(0,string_length,column):
+        matrix.append(s[i:i+row+1])
+    output = ""
+    for i in range(column):
+        to_add = ""
+        for j in range(row):
+            if (i > len(matrix[j]) - 1):
+                break
+            to_add += matrix[j][i]
+        output += to_add + " "
+    print(matrix)
+    return output[:-1]
+    
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     s = input()
 
     result = encryption(s)
 
-    fptr.write(result + '\n')
+    print(result)
 
-    fptr.close()
 
